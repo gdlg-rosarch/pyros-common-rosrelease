3e5ce5435f7a1d4515418c748eba16b2d93e2642
AlexV <asmodehn@gmail.com>
2017-04-19 18:15:45 +0900

    Rebase from 'rpm/indigo/pyros_common'

--- pyros_interfaces_mock/__init__.py
+++ pyros_interfaces_mock/__init__.py
@@ -23,7 +23,7 @@ _logger.addHandler(logging.NullHandler())
 
 try:
     # early except to prevent unintentional workaround in all modules here for packages we depend on
-    import pyros_interfaces.common
+    import pyros_interfaces_common
     # this is only useful when these dependent packages are in a catkin/ROS workspace
     # TODO : detect if we are in a catkin workspace / following a ROS workflow ?
     # TODO : probably better than activate if failure occurs...
@@ -34,7 +34,7 @@ except ImportError:
     # This will load the pyros_setup configuration from the environment
     pyros_setup.configurable_import().configure().activate()
     # validate we can load ROS modules. Note other variables (like ROS_PACKAGE_PATH) should also be available.
-    import pyros_interfaces.common
+    import pyros_interfaces_common
 
 
 from .mockinterface import MockInterface
--- pyros_interfaces_mock/mockpublisher.py
+++ pyros_interfaces_mock/mockpublisher.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from pyros_interfaces.common.transient_if import TransientIf
+from pyros_interfaces_common.transient_if import TransientIf
 
 
 class MockPublisher(TransientIf):
