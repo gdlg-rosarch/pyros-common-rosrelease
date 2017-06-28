66e994669dc86409ccee8bfd3aaf33c9e4d8e316
alexv <asmodehn@gmail.com>
2017-06-28 12:13:57 +0900

    Rebase from 'release/kinetic/pyros_common'

--- travis_checks.bash
+++ travis_checks.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# first we ensure we change to the directory where this script is.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
 # These variables need to be setup before calling this script:
 # CI_ROS_DISTRO [indigo | jade]
 # ROS_FLOW [devel | install]
